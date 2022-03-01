9152830aa9a64731f55631b82c7bc5914be79bb3
maumaumaumaumaumau <maumaumaumaumaumua@gmail.com>
2022-03-01 20:51:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,33 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"))
 })
 
-// yes this is spaghetti code 
+// REDIRECTION
+
+app.get("/index.html", (req, res) => {
+    res.redirect("/")
+})
+
+app.get("/videos/index.html", (req, res) => {
+    res.redirect("/videos")
+})
+
+app.get("/contact.html", (req, res) => {
+    res.redirect("/contact")
+})
+
+// yes this is spaghetti code  ill remake later
+
+app.get("/HUBCHICKEN.png", (req, res) => {
+    res.sendFile(path.join(__dirname, "/HUBCHICKEN.png"))
+})
+
+app.get("/icon.svg", (req, res) => {
+    res.sendFile(path.join(__dirname, "/icon.svg"))
+})
+
+app.get("/style.css", (req, res) => {
+    res.sendFile(path.join(__dirname, "/style.css"))
+})
 
 app.get("/video-array.js", (req, res) => {
     let videos = ""
@@ -50,6 +76,10 @@ app.get("/cdn/:file", (req, res) => {
 
 app.get("/assets/:file", (req, res) => {
     res.sendFile(path.join(__dirname, "assets", req.params.file))
+})
+
+app.use(function(req, res, next) {
+  res.status(404).sendFile(path.join(__dirname, "404.html"))
 })
 
 app.listen(8080)
